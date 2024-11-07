e56ecee534498ced535d16ceba5a6048bde576f9
heathergibb <hrgibb@gmail.com>
2024-11-07 15:40:46 -0700

    finished stretch

--- controllers/professional.js
+++ controllers/professional.js
@@ -1,9 +1,20 @@
+const mongodb = require('../db/connect');
 
 exports.getData = async (req, res, next) => {
     try {
-        const data = require('../user.json');
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(data[0]);
+        // removed for stretch
+        // const data = require('../user.json');
+        // res.setHeader('Content-Type', 'application/json');
+        // res.status(200).json(data[0]);
+
+        // STRETCH
+        const data = await mongodb.getDb().db().collection('user').find();
+        data.toArray().then((lists) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(lists[0]);
+        });
+        // END OF STRETCH
+        
     } catch (err) {
         console.error("Error loading JSON data.");
         res.status(500).json({error: "Failed to retrieve data"});
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
+//stretch activity
+const { MongoClient } = require('mongodb');
+const mongodb = require('./db/connect');
+//end of stretch
+
 const app = express();
 const profRoutes = require('./routes/professional');
 
@@ -12,5 +18,19 @@ app.use((req, res, next) => {
 });
 app.use('/professional', profRoutes);
 
-app.listen(port);
-console.log("Web Server is listening at: " + port);
+// removed for stretch activity
+// app.listen(port);
+// console.log("Web Server is listening at: " + port);
+
+
+//stretch activity
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log("Connected to DB and listening at: " + port);
+    }
+});
+
+//end of stretch
